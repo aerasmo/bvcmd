1cee868b042a1e09f8650b533ae2b82cff1e30b0
aerasmo <angelo1erasmo@gmail.com>
2022-07-01 00:06:21 +0800

    Scrape and copy verse to clipboard

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import argparse
 import json
+import requests
+from bs4 import BeautifulSoup
+import pyperclip 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verse", help="verse snippet", type=str)
+    parser.add_argument("-v", "--verse", help="verse query", type=str)
     args = parser.parse_args()
     return args
 
@@ -39,12 +42,28 @@ def parse_verse(verse: str) -> str:
 
     return (book, chapter, number)
 
-def get_verse(book:str, chapter:str, number:str, version:int=1) -> str: 
-    # URL = [http: // [version]/[book].[verses]]
-    # docu = getDocument(url)
-    # bible_verse = findclass(docu)
-    return "bible verse"
+def get_verse(book:str, chapter:str, number:str, version:int=177) -> str: 
+    if number == "*":
+        url = f"https://www.bible.com/bible/{version}/{book}.{chapter}"
+    else:
+        url = f"https://www.bible.com/bible/{version}/{book}.{chapter}.{number}"
+    data = requests.get(url)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    
+    l1 = soup.find_all("h1", class_="f6 f5-m mb3 yv-gray50") 
+    for i in l1:
+        heading = i.get_text()
+    
+    l2 = soup.find_all("div", class_="yv-gray50 lh-copy f3-m") 
+    for i in l2:
+        verse = i.get_text()
+    return heading, verse
 
+def convert_to_markdown(heading, bible_verse):
+    return f"""# {heading}
+> {bible_verse}
+"""
+    
 def main(verse=None):
     if verse == None:
         verse = input("Enter query: ")
@@ -52,11 +71,14 @@ def main(verse=None):
     book, chapter, number = parse_verse(verse)
     book_abbv = convert_to_keyword(book)
     print(f"book_abbv:", book_abbv)
-    bible_verse = get_verse(book_abbv, chapter, number)
 
-    # copy bible verse to clipboard 
+    heading, bible_verse = get_verse(book_abbv, chapter, number)
+    print(f"copied \"{bible_verse}\" to clipboard ")
+
+    heading = " ".join(heading.split(" ")[:-1])
+    md = convert_to_markdown(heading, bible_verse)
+    pyperclip.copy(md)
     return 
-
 
 if __name__ == "__main__":
     args = get_args()
